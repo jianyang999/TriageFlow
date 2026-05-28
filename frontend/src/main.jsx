0318782f2ec940880c09a8d563f9ba3a2e34e75e
Jian Yang <126151808+jianyang999@users.noreply.github.com>
2026-05-28 23:52:59 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import QueuePage from './pages/QueuePage'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<QueuePage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )

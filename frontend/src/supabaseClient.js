6071c89d1e18a52b563b5f597e108c7b0103d61d
Jian Yang <126151808+jianyang999@users.noreply.github.com>
2026-06-28 11:13:49 +0800

    more test

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://jcsbqnfsutrwotnfywvu.supabase.co',
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impjc2JxbmZzdXRyd290bmZ5d3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMTIwOTEsImV4cCI6MjA5NDY4ODA5MX0.N9nhE1rt_wd6MuyGOGUVVNZzKj9XSvrjyJWGbx_teEk'
 )
